d483f714bba206849d8dc831175e8a3b4ec34e94
juliwithoutthee <jnboucree@gmail.com>
2022-08-26 14:29:30 +0900

    Adds method to Node class

--- algorithms/linkedlist.py
+++ algorithms/linkedlist.py
@@ -3,8 +3,10 @@ class Node:
         self.value = value
         self.next = None
 
+    def __repr__(self):
+        self.value
+
 class LinkedList: 
     def __init__(self):
         self.head = None
 
-        
