eb56572eab66d6f252cfdfc1b62df95211f9f017
juliwithoutthee <jnboucree@gmail.com>
2022-08-27 23:12:16 +0900

    Adds method to insert nodes at the end of the linked list

--- algorithms/linkedlist.py
+++ algorithms/linkedlist.py
@@ -34,3 +34,11 @@ class LinkedList:
     def add_first(self, node):
         node.next = self.head
         self.head = node
+
+    def add_last(self, node):
+        if self.head is None:
+            self.head = node
+            return 
+        for current_node in self:
+            pass 
+        current_node.next = node
