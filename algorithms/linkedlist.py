4613815b6aed50e7bf0dae60aa25fb067634f5f2
juliwithoutthee <jnboucree@gmail.com>
2022-08-29 14:33:23 +0900

    Adds a method to insert node after target node

--- algorithms/linkedlist.py
+++ algorithms/linkedlist.py
@@ -42,3 +42,15 @@ class LinkedList:
         for current_node in self:
             pass 
         current_node.next = node
+
+    def add_after(self, target_node_data, new_node):
+        if self.head is None: 
+            raise Exception("List is Empty")
+
+        for node in self:
+            if node.data == target_node_data:
+                new_node.next = node.next
+                node.next = new_node 
+                return 
+
+        raise Exception("Node with data '%s' not found" % target_node_data)
