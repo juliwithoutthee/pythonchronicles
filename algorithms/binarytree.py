e410da5f8dcb53c08e7802bba93a548422625f99
juliwithoutthee <jnboucree@gmail.com>
2022-08-30 16:33:35 +0900

    Creates a basic binary tree

--- algorithms/binarytree.py
+++ algorithms/binarytree.py
@@ -5,4 +5,35 @@ class Node:
         self.right = None 
         self.value = value
 
+    def traversePreOrder(self):
+        print(self.value, end=' ')
+        if self.left:
+            self.left.traversePreOrder()
+        if self.right:
+            self.right.traversePreOrder()
 
+    def traverseInOrder(self):
+        if self.left:
+            self.left.traverseInOrder()
+        print(self.value, end=' ')
+        if self.right:
+            self.right.traverseInOrder()
+
+    def traversePostOrder(self):
+        if self.left: 
+            self.left.traversePostOrder()
+        if self.right: 
+            self.right.traversePostOrder()
+        print(self.value, end=' ')
+
+root = Node(1)
+
+root.left = Node(2)
+root.right = Node(3)
+
+print("Pre order Traversal: ", end="")
+root.traversePreOrder()
+print("\nIn order Traversal: ", end="")
+root.traverseInOrder()
+print("\nPost order Traversal: ", end="")
+root.traversePostOrder()
