b81e9983b2901fdfa765d1368b1619481fac4437
juliwithoutthee <jnboucree@gmail.com>
2022-08-30 12:11:14 +0900

    Adds test method for add_last method of LinkedList

--- algorithms/test_linkedlist.py
+++ algorithms/test_linkedlist.py
@@ -6,3 +6,6 @@ class TestLinkedList:
     def test_add_first(linkedlist):
         
         assert LinkedList.add_first()
+
+    def test_add_last(linkedlist):
+        assert LinkedList.add_last()
