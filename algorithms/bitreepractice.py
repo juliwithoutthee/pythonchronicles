4b42a49deae1c171a5259aae82ba93e16a0626f0
juliwithoutthee <jnboucree@gmail.com>
2022-09-01 18:33:03 +0900

    Updates the problem comments

--- algorithms/bitreepractice.py
+++ algorithms/bitreepractice.py
@@ -1,8 +1,12 @@
 # pointer to root of binary tree and two values v1 v2
 # return the lowest ancestor of v1 and v2 in binary search tree 
 # v1 v2 are int and exist in the tree 
-# check for null? 
+# Check for null? 
+# No duplicate numbers
 # Depth First Search 
+
+from logging import _Level
+
 
 class Solution: 
     def __init__(self) -> None:
@@ -10,6 +14,20 @@ class Solution:
 
 
 class Node:
-    def __init__(self):
+    lvl = 0
+    def __init__(self, value):
         self.left = None
         self.right = None
+        self.value = value
+
+    def getLeft(self):
+        return self.left
+
+    def getRight(self):
+        return self.right
+
+    def getData(self, node):
+        return self.value 
+    
+    def getLevel(self, v1, v2):
+        self
