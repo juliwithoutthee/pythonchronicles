c7673cefe8fc517985d7391ea5765b8afe52ca6e
juliwithoutthee <jnboucree@gmail.com>
2022-09-01 18:24:24 +0900

    Updates the practice problem

--- algorithms/bitreepractice.py
+++ algorithms/bitreepractice.py
@@ -1,2 +1,15 @@
 # pointer to root of binary tree and two values v1 v2
 # return the lowest ancestor of v1 and v2 in binary search tree 
+# v1 v2 are int and exist in the tree 
+# check for null? 
+# Depth First Search 
+
+class Solution: 
+    def __init__(self) -> None:
+        pass
+
+
+class Node:
+    def __init__(self):
+        self.left = None
+        self.right = None
