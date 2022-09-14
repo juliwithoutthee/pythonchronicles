5e7608785c2472d583e3ecad9a16c1a55d68d002
juliwithoutthee <jnboucree@gmail.com>
2022-09-14 16:52:42 +0900

    Creates a list comprehension using strings

--- general/list_comprehension.py
+++ general/list_comprehension.py
@@ -2,3 +2,9 @@
 sloths = ["linneas", "hoffman", "pygmy", "pale-throat", "brown-throat", "maned"]
 
 [print(sloth.capitalize()) for sloth in sloths]
+
+sloth_string = "HelloSlothsOfTheWorld"
+
+sloth_string= "".join([i if i.islower() else " " + i for i in sloth_string])[1:]
+
+print(sloth_string)
