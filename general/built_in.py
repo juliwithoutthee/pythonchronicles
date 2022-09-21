71bda612d491bf591e4a8bc523806371e3da8980
juliwithoutthee <jnboucree@gmail.com>
2022-09-21 09:43:58 +0900

    Adds example for any()

--- general/built_in.py
+++ general/built_in.py
@@ -1,6 +1,16 @@
 # Examples of built in python functions
 
-# abs()  returns the absolute value of a given number
+# abs() returns the absolute value of a given number
+from tomlkit import boolean
+
+
 number = -15 
 abs(number)
 
+
+# any() returns True if any element of an iterable is True 
+boolean_list = ['False', 'True', 'False']
+result = any(boolean_list)
+
+
+#
