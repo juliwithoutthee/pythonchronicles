6a5d9d832f23b90863d5546a7e346d519055a23b
juliwithoutthee <jnboucree@gmail.com>
2022-09-22 10:49:39 +0900

    Adds example for bytearray()

--- general/built_in.py
+++ general/built_in.py
@@ -21,3 +21,15 @@ result2 = all(bool_list)
 # bool() returns a boolean value for a specified argument
 print(bool(0))
 print(bool(1))
+
+
+# bytearray() returns a bytearray object which is an array of given bytes
+prime_numbers = [2, 3, 5, 7]
+# convert list to bytearray
+byte_array = bytearray(prime_numbers)
+print(byte_array)
+
+# Output: bytearray(b'\x02\x03\x05\x07')
+
+
+
