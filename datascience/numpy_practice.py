afcfee6fa9b20dea73eac85aa765fdc995030123
juliwithoutthee <jnboucree@gmail.com>
2022-11-03 16:13:13 +0900

    Add example arrays using numpy methods

--- datascience/numpy_practice.py
+++ datascience/numpy_practice.py
@@ -2,3 +2,15 @@ import numpy as np
 
 random_int = np.random.randint(1,100)
 print(random_int)
+
+
+new_array = np.arange(0,10,2)
+print(new_array)
+
+# creates an array in a matrix format 
+new_matrix = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(new_matrix)
+
+# 5x5 matrix of zeros 
+zero_matrix = np.zeros((5,5))
+print(zero_matrix)
