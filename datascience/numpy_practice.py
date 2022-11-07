d3f4789cf2312b628c8be8d68b3f071de9dd8a8a
juliwithoutthee <jnboucree@gmail.com>
2022-11-07 15:47:05 +0900

    Add examples for numpy array and sorting notation

--- datascience/numpy_practice.py
+++ datascience/numpy_practice.py
@@ -14,3 +14,17 @@ print(new_matrix)
 # 5x5 matrix of zeros 
 zero_matrix = np.zeros((5,5))
 print(zero_matrix)
+
+
+# Create an array with values from 10-50
+ten_to_fifty = np.arange(10,51)
+print(ten_to_fifty)
+#array([10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+#       27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43,
+#       44, 45, 46, 47, 48, 49, 50])
+ten_to_fifty[ten_to_fifty%2 == 0]
+
+# create the output array:  array([ 5.,  5.,  5.,  5.,  5.,  5.,  5.,  5.,  5.,  5.])
+arr_fives = np.arange(1,11) 
+arr_fives[:] = 5.
+print(arr_fives)
