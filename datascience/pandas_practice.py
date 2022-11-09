64ee556a5df214688b111d081106a9ec712b776e
juliwithoutthee <jnboucree@gmail.com>
2022-11-09 11:47:13 +0900

    Add new column to df

--- datascience/pandas_practice.py
+++ datascience/pandas_practice.py
@@ -7,4 +7,10 @@ np.random.seed(101)
 
 df = pd.DataFrame(randn(5,4), ['A', 'B', 'C', 'D', 'E'], ['W', 'X', 'Y', 'Z'])
 
+print(df['Z'])  # df['Z'] grabs column, df.Z is also possible but not recommended
+
+print(df[['W', 'X']])  # Use list to get multiple columns
+
+df['New'] = df['X'] + df['Y']  # Add new column to df
+
 print(df)
