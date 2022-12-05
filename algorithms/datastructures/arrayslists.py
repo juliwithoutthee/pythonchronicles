0dd5de6b10d78d78a6eb0193507634bf3990d231
juliwithoutthee <jnboucree@gmail.com>
2022-12-05 17:34:56 +0900

    Add function reverse_array

--- algorithms/datastructures/arrayslists.py
+++ algorithms/datastructures/arrayslists.py
@@ -1,3 +1,4 @@
+from tracemalloc import start
 import numpy as np 
 
 
@@ -26,3 +27,19 @@ print(min)  # O(n) linear search
 numpy_array = np.array(array)
 
 print(numpy_array)
+
+
+# Reverse Array in Place (reverse list)
+# input: [1,2,3] output: [3,2,1]
+def reverse_array(arr):
+    start_index = 0
+    end_index = len(arr) -1
+
+    while start_index < end_index:
+        arr[start_index], arr[end_index] = arr[end_index], arr[start_index]
+        start_index = start_index + 1
+        end_index = end_index - 1
+    return arr
+    
+
+print(reverse_array([1,2,3]))
