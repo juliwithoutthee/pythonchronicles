e16c85d76e8143c3414cde205abdfacb5a36fec4
juliwithoutthee <jnboucree@gmail.com>
2022-12-20 11:58:08 +0900

    Update stepping_lists generator

--- general/generators.py
+++ general/generators.py
@@ -6,17 +6,36 @@ def steps(start, stop):
 
 odds = steps(3,15)
 
-print(next(odds))
-print(next(odds))
-print(next(odds))
-print(next(odds))
-print(next(odds))
-print(next(odds))
-print(next(odds))
+# print(next(odds))
+# print(next(odds))
+# print(next(odds))
+# print(next(odds))
+# print(next(odds))
+# print(next(odds))
+# print(next(odds))
 
 evens = steps(2,10)
-print(next(evens))
-print(next(evens))
-print(next(evens))
-print(next(evens))
-print(next(evens))
+# print(next(evens))
+# print(next(evens))
+# print(next(evens))
+# print(next(evens))
+# print(next(evens))
+
+
+# generator for steps in array 
+# input iterator and step through indecies to access values 
+
+def stepping_lists(lst, step):
+    for i in range(0, len(lst), step):
+        yield lst[i]
+    yield lst[::step]
+
+a = [0,1,2,3,4,5,6]
+stepa = stepping_lists(a, 2) 
+print(stepa)
+print(next(stepa))
+print(next(stepa))
+print(next(stepa))
+print(next(stepa))
+print(next(stepa))
+
